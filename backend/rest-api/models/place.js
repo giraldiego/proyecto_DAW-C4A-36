86f38e92d03b5b58df0c8b6e6d94472b27723457
giraldiego <giraldiego@gmail.com>
2021-11-26 05:58:02 -0500

    Add enum validators to Schemas

--- backend/rest-api/models/place.js
+++ backend/rest-api/models/place.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// TODO: create enum for options
+const TYPE = ['apartamento', 'casa', 'local'];
+const OFFER_TYPE = ['alquiler', 'venta'];
+const STATUS = ['activo', 'inactivo', 'comprado', 'ocupado'];
 
 // Create the Schema for a Place
 const placeSchema = new Schema({
   // department: { type: String, required: true },
   city: { type: String, required: true },
   // address: { type: String, required: true },
-  type: { type: String, required: true }, // apartamento, casa, local
-  offerType: { type: String, required: true },  // alquiler, venta
+  type: {
+    type: String,
+    enum: TYPE,
+    required: true,
+  }, // apartamento, casa, local
+  offerType: { type: String, enum: OFFER_TYPE, required: true }, // alquiler, venta
   // price: {type: Number, required: true},
-  status: { type: String, default: 'activo' }, // activo, inactivo, comprado, ocupado
+  status: {
+    type: String,
+    enum: STATUS,
+    default: 'activo',
+  }, // activo, inactivo, comprado, ocupado
   // pictures: [String],
   // urlVideo: String,
   // contactInfo: String,
--- backend/rest-api/models/user.js
+++ backend/rest-api/models/user.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 // const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
-// TODO: create enum for options
+const ROLE = ['cliente', 'asesor', 'admin'];
 
 const userSchema = new Schema({
-    name:{ type: String, required: true},
-    email:{ type: String, required: true, unique: true},
-    password:{ type: String, required: true, minlength: 6 },
-    role: {type:String, default: 'cliente'},    // values: admin, asesor, *cliente
-    emailVerified: {type:Boolean, default: false},
-    verificationToken: String,
-    requests: [{ type: mongoose.Types.ObjectId, ref: 'Request' }],
-    places: [{ type: mongoose.Types.ObjectId, ref: 'Place' }], // Only aplies to Salesman
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  role: {
+    type: String,
+    enum: ROLE, // values: admin, asesor, *cliente
+    default: 'cliente',
+  },
+  emailVerified: { type: Boolean, default: false },
+  verificationToken: String,
+  requests: [{ type: mongoose.Types.ObjectId, ref: 'Request' }],
+  places: [{ type: mongoose.Types.ObjectId, ref: 'Place' }], // Only aplies to Salesman
 });
 
 // Apply the uniqueValidator plugin to userSchema.
