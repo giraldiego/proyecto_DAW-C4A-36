bbcddddcc81dc32f38dcc10cee503cd55ee86c58
giraldiego <giraldiego@gmail.com>
2021-12-05 07:14:06 -0500

    Setup CORS policy to allow access from localhost

--- backend/rest-api/server.js
+++ backend/rest-api/server.js
@@ -18,11 +18,11 @@ const HttpError = require('./models/http-error');
 // allowing CORS for all resources on your server.
 const cors = require('cors');
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(
+    cors({
+        origin: 'http://localhost:4200',
+    })
+);
 
 // Add json parser
 app.use(express.json());
