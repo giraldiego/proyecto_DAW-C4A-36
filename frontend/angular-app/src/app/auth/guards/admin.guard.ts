f3ecd6711517994f76f048fa0dbe49a3e6543afc
giraldiego <giraldiego@gmail.com>
2021-12-05 23:52:32 -0500

    Add canActivateChild to AdminGuard

--- frontend/angular-app/src/app/auth/guards/admin.guard.ts
+++ frontend/angular-app/src/app/auth/guards/admin.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class AdminGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private token: TokenStorageService,
@@ -14,6 +14,11 @@ export class AdminGuard implements CanActivate {
     ) {}
 
   canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.token.isAdmin() || this.router.navigate(['/home']);
+  }
+  canActivateChild(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.token.isAdmin() || this.router.navigate(['/home']);
