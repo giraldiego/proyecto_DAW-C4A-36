4436520276622f77bada4dd085aac14cb5bb1b92
giraldiego <giraldiego@gmail.com>
2021-12-05 23:33:44 -0500

    Add admin guard and protect /admin/users route

--- frontend/angular-app/src/app/admin/admin-routing.module.ts
+++ frontend/angular-app/src/app/admin/admin-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminGuard } from '../auth/guards/admin.guard';
 import { PlaceListComponent } from './places/place-list/place-list.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 
 const routes: Routes = [
-  { path: 'users', component: UserListComponent },
+  { path: 'users',
+    component: UserListComponent,
+    canActivate: [AdminGuard] },
+
   { path: 'places', component: PlaceListComponent }
 ];
 
